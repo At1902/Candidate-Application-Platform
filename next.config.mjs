2d2b985a75b09d4a2fab547704399dcd2c78c66c
At1902 <atishay083@gmail.com>
2024-05-06 17:11:56 +0530

    Enable image rendering from 'logo.clearbit.com' in nextjs config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "logo.clearbit.com",
+        port: "",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
