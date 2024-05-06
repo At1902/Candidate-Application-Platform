1d3654a3bcec311ee88cf31f81d14b4f108ef44a
At1902 <atishay083@gmail.com>
2024-05-06 23:17:23 +0530

    Add not-found page to handle an unreachable route

---
+++ app/not-found.js
@@ -0,0 +1,13 @@
+export default function NotFound() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <h1>Oops! It seems like the page you are looking for does not exist</h1>
+    </div>
+  );
+}
