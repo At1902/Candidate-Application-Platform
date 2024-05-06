878da8c8aeece941f573f58ce634c752b5f82e57
At1902 <atishay083@gmail.com>
2024-05-06 22:38:48 +0530

    Add styling for homepage

--- app/page.js
+++ app/page.js
@@ -1,10 +1,11 @@
 import Filters from "@components/Filters";
 import JobList from "@components/JobList";
+import style from "@styles/HomePage.module.css";
 
 export default function Home() {
   return (
     <main>
-      <h1>Candidate Application Platform</h1>
+      <h1 className={style.title}>Candidate Application Platform</h1>
       <Filters />
       <JobList />
     </main>
